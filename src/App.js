81d62ae27d24f7239516a0a8b6d1590261571305
surya <suryanarayana@desynova.com>
2025-09-16 15:58:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from GitHub Actions 🚀</h1>
+      <p>This is a simple React app deployed via CI/CD.</p>
+    </div>
+  );
+}
+
+export default App;
